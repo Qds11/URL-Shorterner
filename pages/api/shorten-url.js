15317b64778e1fe4fd413eac0f28fea523ebec1d
Lim Qi Qing <101784318+Qds11@users.noreply.github.com>
2024-08-01 23:06:21 +0800

    test: returning some dummy data

--- pages/api/shorten-url.js
+++ pages/api/shorten-url.js
@@ -4,35 +4,7 @@ import { validateUrl } from "../../utils/url_validation";
 
 
 export default async function handler(req, res) {
-  try {
-    const { longurl } = req.body;
-    const { BASE_URL } = process.env;
+   console.log("Entered the serverless function");
 
-    if (!validateUrl(longurl)) {
-      return res.status(400).json({ error: "Invalid URL format." });
-    }
-
-    const db = await pool.getConnection();
-  
-    let key;
-    do {
-      key = crypto.randomBytes(3).toString("hex");
-    } while (
-      (
-        await db.query(
-          "SELECT COUNT(*) FROM url_information WHERE short_url_key=?",
-          [key]
-        )
-      )[0]["COUNT(*)"] > 0
-    );
-  
-    const query =
-      "INSERT INTO url_information (short_url_key, long_url) VALUES (?, ?)";
-    await db.query(query, [key, longurl]);
-      db.release();
-      res.status(200).json({ shortUrl: `${BASE_URL}/api/${key}` });
-  } catch(error){
-     console.error("Error shortening url:", error);
-     res.status(500).json({ error: "Internal Server Error." });
-  }
+   return { dummy: "data" };
 }
